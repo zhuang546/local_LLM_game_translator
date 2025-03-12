09c918e34280e7bf7c1704219d09d578944b3356
ZHUANG ZONGHUAN <44138486+zhuang546@users.noreply.github.com>
2025-03-12 19:44:46 +0900

    Update OllamaTranslationClient.py

--- OllamaTranslationClient.py
+++ OllamaTranslationClient.py
@@ -1,24 +1,21 @@
 from ollama import Client
 from ollama import list as ollama_list
 from OllamaServer import OllamaServer
-from OllamaServer import pause_exit
 
 '''
 OllamaTranslationClient类的主要职能：
-1. 通过OllamaServer类，启动Ollama服务器
-2. 通过set_model方法，选择模型
-3. 接收外界传递的参数，翻译文本
+1. 通过set_model方法，选择模型
+2. 接收外界传递的参数，翻译文本
 额外的功能：
 1. 提供message_head_extend方法，通过外部类追加对话历史
 翻译时的机制：
 1. 对话头：维护一个包含固定system消息和预设对话历史的列表，用于指导翻译
-3. 对话历史：维护一个具有一定长度的动态的对话历史列表，用于保持翻译的连贯性
+2. 对话历史：维护一个具有一定长度的动态的对话历史列表，用于保持翻译的连贯性
 '''
 
 class OllamaTranslationClient:
     def __init__(self, num_predict, temperature, stop, history_max_length, clear_history, system_message):
         
-        self.olla_manager = OllamaServer() # 启动Ollama服务器
         self.client = Client() # 创建Ollama客户端
         self.model = ''
         self.set_model() # 选择模型
@@ -39,9 +36,9 @@ class OllamaTranslationClient:
         try:
             models = ollama_list().models
         except Exception as e:
-            pause_exit(e)
+            OllamaServer.sys_exit(e)
         if not models:
-            pause_exit('未找到可用模型！')
+            OllamaServer.sys_exit('未找到可用模型！')
         print('------------------------------------')
         print('可用模型列表：')
         for model in models:
@@ -57,7 +54,7 @@ class OllamaTranslationClient:
                 model_found = True
                 break
         if not model_found:
-            pause_exit('模型名称错误！')
+            OllamaServer.sys_exit('模型名称错误！')
         print('已选择模型：', model_name)
         self.model = model_name
 
@@ -72,7 +69,7 @@ class OllamaTranslationClient:
                     {'role': 'assistant', 'content': translated_text}
                     ]
         except Exception as e:
-            pause_exit('追加对话头失败：', e)
+            OllamaServer.sys_exit('追加对话头失败：', e)
 
     def translate(self, text):
         if not text: # 空文本直接返回
@@ -102,17 +99,17 @@ class OllamaTranslationClient:
                     else:
                         self.messages = self.messages[-self.history_max_length // 2 * 2:] # 缩短对话历史，连贯性可能更好，但错误可能会一直传递
         except Exception as e:
-            pause_exit(e)
+            OllamaServer.sys_exit(e)
         return tranlated_text
     
     def run(self):
         # 测试
-        while True:
-            text = input('请输入要翻译的文本：')
-            print('翻译结果：', self.translate(text))
-            print('messages数目：', len(self.messages)//2)
+        text = input('请输入要翻译的文本：')
+        print('翻译结果：', self.translate(text))
+        print('messages数目：', len(self.messages)//2)
 
 if __name__ == "__main__":
+    OllamaServer.start_ollama_server()
     # 测试
     translator = OllamaTranslationClient(
         num_predict=200,
@@ -130,3 +127,4 @@ if __name__ == "__main__":
 '''
     )
     translator.run()
+    OllamaServer.sys_exit()
