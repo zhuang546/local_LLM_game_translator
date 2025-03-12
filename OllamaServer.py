502fd64a42b34b09288c40a0e463bfa2c0660145
ZHUANG ZONGHUAN <44138486+zhuang546@users.noreply.github.com>
2025-03-12 19:44:14 +0900

    Update OllamaServer.py

--- OllamaServer.py
+++ OllamaServer.py
@@ -1,63 +1,49 @@
-# installed packages: psutil
+# 构建命令：pyinstaller -F --clean OllamaServer.py
 import subprocess
 import time
 import psutil
 from sys import exit
-
-def pause_exit(error_message=''):
-    print(error_message)
-    input("按回车继续...")
-    exit()
+from os import getpid
 
 '''
 OllamaServer 类：
 用于启动和关闭 Ollama 服务器
 '''
 class OllamaServer:
-    def __init__(self):
-        self.ollama_process = None
-        self.start_ollama_server()
-
-    def __del__(self):
-        # 在类被销毁时关闭 Ollama 服务器，这种方法不稳定，但胜在简单
-        self.stop_ollama_server()
-
-    def start_ollama_server(self):
+    @staticmethod
+    def start_ollama_server():
         try:
-            self.ollama_process = subprocess.Popen(
+            server_process = subprocess.Popen(
                 ["ollama", "serve"],
                 stdout=subprocess.DEVNULL, # 关闭控制台输出
                 stderr=subprocess.DEVNULL
             )
             time.sleep(2)
-            print("Ollama 服务器已启动")
+            print("Ollama 服务器已启动，pid：",server_process.pid)
         except Exception as e:
             print(f"启动 Ollama 失败: {e}")
 
-    def stop_ollama_server(self):
+    @staticmethod
+    def stop_ollama_server():
         # 使用 psutil 来终止进程，这种方法具有很好的跨平台特性
-        if self.ollama_process is None:
-            return
         try:
-            parent = psutil.Process(self.ollama_process.pid)
-            children = parent.children()  # 模型运行时ollama.exe服务会启动其他ollama.exe进程
-
-            # 先终止父进程（主 ollama.exe）
-            parent.terminate()
-            parent.wait(timeout=5)
-
-            # 如果子进程还在，则终止它
-            if children: # 如果有二级及以下进程，需要更复杂的遍历，但这里省去
-                for child in children:
-                    if child.is_running():
-                        child.terminate()
-                        child.wait(timeout=5)
+            parent = psutil.Process(getpid())
+            # 递归终止子进程
+            for child in parent.children(recursive=True):
+                child.terminate()
+                child.wait(timeout=5)
+                print("Ollama 服务器已停止，pid：", child.pid)
         except Exception as e:
             print(f"终止 Ollama 服务器失败: {e}")
-        else:
-            print("Ollama 服务器已停止")
+
+    @staticmethod
+    def sys_exit(error_message=''):
+        print(error_message)
+        OllamaServer.stop_ollama_server()
+        input("按回车退出...")
+        exit()
 
 if __name__ == "__main__":
-    # 测试代码
-    managment = OllamaServer()
-    pause_exit()
+    # 测试
+    OllamaServer.start_ollama_server()
+    OllamaServer.sys_exit()
