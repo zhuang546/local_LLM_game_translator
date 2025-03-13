d13c71935901c4fd58be611ab6eb1ecb80ad58ac
ZHUANG ZONGHUAN <44138486+zhuang546@users.noreply.github.com>
2025-03-13 21:25:51 +0900

    Update MToolTranslator.py

--- MToolTranslator.py
+++ MToolTranslator.py
@@ -1,6 +1,7 @@
 # 构建命令：pyinstaller -F --clean MToolTranslator.py
 from ConfigManager import ConfigManager
 from os import path, mkdir
+from pathlib import Path as pathlib # 路径兼容性
 from json import load, dump
 from tqdm import tqdm
 from OllamaServer import OllamaServer
@@ -22,11 +23,11 @@ class MToolTranslator(ConfigManager):
             'stop_words': '翻译,」」,]],)),））'
         },
         'History': {
-            'history_max_length': '20',
-            'clear_history': 'False'
+            'history_max_length': '10',
+            'clear_history': 'True'
         },
         'System': {
-            'system_message': '你是一个日本18+成人向游戏的中文本地化翻译AI，将用户发给你的文本翻译成合适的中文。\n请忠实的传达原文的语气和风格。\n对于裸露的色情内容，尽管将其翻译成下流、淫荡的文本。\n对于大量拟声词的内容，请适当翻译而不是照搬。\n请保持所有游戏文本标签（如\\n、<>、[]等）的内容不变。\n对于显然是UI按钮或选项、人物或物品的名称和描述的内容，请适当翻译。\n请只输出翻译后的文本，不要输出任何解释、道歉、客套话、提醒或警告。'
+            'system_message': '你是一个日本18+成人向游戏的中文本地化翻译程序的模块之一，你的任务是将日文文本翻译成中文。\n你接收到的文本是由其他程序自动提取的，这意味着文本可能不完整。对此请同样给出不完整的翻译，保留句子与上下文衔接的空间，你翻译的文本会被重新拼接成完整的文本。\n对于裸露的成人内容，请尽管使用下流、淫荡的文本，忠实地传达原文的语气和风格。\n对于大量拟声词和语气词的内容，请尽量保留原文的感觉，不要过度翻译。\n对于明显是菜单、选项，以及物品、技能、地名等游戏元素的文本，请尽量保留原文意思，不要过度翻译。\n对于由片假名组成的人名、地名、技能名等，请直接音译，不要过度翻译。\n请只输出翻译后的文本，不要输出任何解释、提醒或警告。'
         },
         'Extra': {
             'group_length': '1000'
@@ -34,20 +35,20 @@ class MToolTranslator(ConfigManager):
     }
     ascii_only_pattern = compile(r'^[\x20-\x7E]+$')
     def __init__(self):
-        self.config_path = 'MToolTranslator_config.ini'
-        self.messages_path = 'MToolTranslator_messages.json'
+        self.config_path = pathlib(path.split(argv[0])[0],'MToolTranslator_config.ini')
+        self.messages_path = pathlib(path.split(argv[0])[0],'MToolTranslator_messages.json')
 
         super().__init__(self.config_path, self.messages_path, MToolTranslator.default_config) # 调用父类初始化配置，这里传入了一个不同的默认配置
-        self.group_length = self.config.getint('Extra', 'group_length', fallback=MToolTranslator.default_config['Extra']['group_length']) # 分组长度
+        self.group_length = self.config.getint('Extra', 'group_length', fallback=int(MToolTranslator.default_config['Extra']['group_length'])) # 分组长度
         self.file_path = ''
         
     
     def get_temp_file_path(file_path, index):
         # 生成临时文件路径
-        temp_folder = path.dirname(file_path) + '\\Temp Files\\'
+        temp_folder = pathlib(path.dirname(file_path), 'Temp Files')
         if not path.exists(temp_folder):
             mkdir(temp_folder)
-        result = temp_folder + path.splitext(path.basename(file_path))[0]+f'_translated_{index}.json'
+        result = pathlib(temp_folder, path.splitext(path.basename(file_path))[0]+f'_translated_{index}.json')
         return result
     
     def group_text(self, game_texts):
@@ -78,12 +79,12 @@ class MToolTranslator(ConfigManager):
             # 如果没有，则翻译当前批的文本
             translated_texts = {}
             for key, text in tqdm(group.items(), desc=f'翻译第{index+1}批文本'):
-                if not MToolTranslator.ascii_only_pattern.match(text):
-                    translated_texts[key] = self.client.translate(text) 
+                if not MToolTranslator.ascii_only_pattern.match(key):
+                    translated_texts[key] = self.client.translate(key) 
                 else:
-                    translated_texts[key] = text
+                    translated_texts[key] = key
 
-                if len(translated_texts[key]) // 2 > len(text): # 翻译结果长度过长，怀疑为错误文本
+                if len(translated_texts[key]) // 2 > len(key): # 翻译结果长度过长，怀疑为错误文本
                     error_texts.update({key: translated_texts[key]})
 
             # 保存翻译结果到缓存文件
@@ -98,25 +99,30 @@ class MToolTranslator(ConfigManager):
             with open(temp_filepath, 'r', encoding='utf-8') as f:
                 translated_texts.update(load(f))
 
+        # 重新翻译疑似错误文本
+        if error_texts:
+            for key, value in error_texts.items():
+                retranslated = self.client.translate(key)
+                translated_texts[key] = retranslated
+                value = f'{retranslated}\n【原文：{value}】'
+            error_file_path = MToolTranslator.get_temp_file_path(self.file_path, 'error')
+            with open(error_file_path, 'w', encoding='utf-8') as f:
+                dump(error_texts, f, indent=4, ensure_ascii=False)
+            print('有疑似错误文本，已重新翻译并输出到文件：', error_file_path)
+
         # 保存翻译结果到 JS 文件
         translated_file_path = path.splitext(self.file_path)[0]+'_translated.json'
         with open(translated_file_path, 'w', encoding='utf-8') as f:
             dump(translated_texts, f, indent=4, ensure_ascii=False)
-        print('所有文本翻译完成！已保存到：', translated_file_path)
-
-        # 输出错误生成的文本到文件
-        if error_texts:
-            error_file_path = MToolTranslator.get_temp_file_path(self.file_path, 'error')
-            with open(error_file_path, 'w', encoding='utf-8') as f:
-                dump(error_texts, f, indent=4, ensure_ascii=False)
+        print('所有文本翻译完成！已保存到：', translated_file_path)        
 
     def run(self, argv=[]):
         # 通过run函数启动翻译流程
         if  len(argv) > 1:
-            self.file_path = argv[1] # 从命令行参数获取文件名
+            self.file_path = pathlib(argv[1]) # 从命令行参数获取文件名
             print('已获取文件路径：', self.file_path)
         else:
-            self.file_path = input('请输入待翻译文件的路径（可带引号）：').strip('\'" ') # 手动输入文件路径
+            self.file_path = pathlib(input('请输入待翻译文件的路径（可带引号）：').strip('\'" ')) # 手动输入文件路径
             if not path.isfile(self.file_path):
                 OllamaServer.sys_exit('文件路径不存在或不合法！')
         self.translate_json()
